f4efea3823995241f9c584718411052a5e811098
MaybeLL <17862720838@163.com>
2021-05-21 14:59:13 +0800

    1

--- foo.py
+++ foo.py
@@ -1,6 +1,19 @@
 
-def f():
-    print("Hello, world!")
+# 谱聚类
+
+
+
+import numpy as np
+
+class spectral():
+    def __init__(self,W,k) -> None:
+        self.W = W
+        self.k = k
+
+    def run():
+        pass
 
 if __name__ == '__main__':
-        print('Hello, main!')
+        W = np.random.normal(loc=0, scale=1, size=[3,3])
+        print(W)
+        
